21a6b7ad7936eec7923fa47d6c3b32088a5f4c5c
rinjugatla <rin.jugatla@gmail.com>
2020-09-26 12:00:04 +0900

    プラグインの基本設定とパッチ処理を追加

--- SkillListGeneratePlugin/SkillListGeneratePlugin/Main.cs
+++ SkillListGeneratePlugin/SkillListGeneratePlugin/Main.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using BepInEx;
+using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SkillListGeneratePlugin
 {
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
     public class Main
     {
+        private const string PluginGuid = "me.rin_jugatla.craftopia.mod.ItemListGenerater";
+        private const string PluginName = "ItemListGenerater";
+        private const string PluginVersion = "1.0.0";
+
+        void Awake()
+        {
+            UnityEngine.Debug.Log($"{PluginName} : {PluginVersion}");
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
     }
 }
