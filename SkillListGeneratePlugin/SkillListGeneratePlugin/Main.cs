c5d59baba7f4b83ba4f2ca6d29f236edf96b03d2
rinjugatla <rin.jugatla@gmail.com>
2020-09-26 12:15:24 +0900

    プラグインにスキルリストファイルを出力する処理を追加

--- SkillListGeneratePlugin/SkillListGeneratePlugin/Main.cs
+++ SkillListGeneratePlugin/SkillListGeneratePlugin/Main.cs
@@ -1,25 +1,64 @@
 ﻿using BepInEx;
 using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Oc;
+using Oc.Skills;
+using SR;
+using System.IO;
 using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SkillListGeneratePlugin
 {
     [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
-    public class Main
+    public class Main : BaseUnityPlugin
     {
-        private const string PluginGuid = "me.rin_jugatla.craftopia.mod.ItemListGenerater";
-        private const string PluginName = "ItemListGenerater";
+        private const string PluginGuid = "me.rin_jugatla.craftopia.mod.SkillListGeneratePlugin";
+        private const string PluginName = "SkillListGeneratePlugin";
         private const string PluginVersion = "1.0.0";
+
+        private const string SkillLogFilepath = @".\Log\SkillList:LANGUAGE.tsv";
 
         void Awake()
         {
-            UnityEngine.Debug.Log($"{PluginName} : {PluginVersion}");
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        /// <summary>
+        /// スキルリストをログファイルに出力
+        /// </summary>
+        [HarmonyPatch(typeof(OcSkillManager), "Start")]
+        public class OcSkillManagerLogger
+        {
+            static void Postfix(OcSkillManager __instance)
+            {
+                if (!Directory.Exists("Log"))
+                    Directory.CreateDirectory("Log");
+
+                // 出力する言語
+                string[] language = new string[] { "English", "Chinese (Simplified)", "Japanese" };
+                string[] filepathFix = new string[] { "En", "Cn", "Jp" };
+
+                string header = "ID\tCategory\tCategoryName\tTier\tSkillName\tMaxLevel\tDescription\tIconName";
+                for (int i = 0; i < language.Length; i++)
+                {
+                    LanguageManager.ChangeLanguage(language[i]);
+                    SoSkillDataList skillList = Traverse.Create(__instance).Field("skillList").GetValue<SoSkillDataList>();
+                    OcSkill[] skills = skillList.GetAll();
+
+                    using (StreamWriter sw = new StreamWriter(SkillLogFilepath.Replace(":LANGUAGE", filepathFix[i]), false, Encoding.UTF8))
+                    {
+                        sw.WriteLine(header);
+                        foreach (var skill in skills)
+                        {
+                            // 未実装のスキルは飛ばす
+                            if (skill.Category == OcPlSkillCategory.None)
+                                continue;
+
+                            sw.WriteLine($"{skill.ID}\t{skill.Category}\t{skill.SkillCategoryName}\t{skill.Tier}\t{skill.SkillName}\t{skill.MaxLevel}\t{skill.OriginDesc}\t{skill.SkillIcon.name}");
+                        }
+                    }
+                }
+            }
         }
     }
 }
